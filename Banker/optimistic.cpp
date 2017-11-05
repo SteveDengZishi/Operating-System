287fe55673047c5581494802211084783fa0ba1b
SteveDengZishi <zd475@nyu.edu>
2017-11-05 01:03:03 -0400

    Banker first commit

---
+++ Banker/optimistic.cpp
@@ -0,0 +1,65 @@
+#include <iostream>
+#include <cstdio>
+#include <vector>
+#include <string>
+#include <cmath> // To use sqrt()
+#include <list> // To use STL linked-list in dfs topological sort
+#include <stack> // To use STL stack in topological sort
+#include <queue> // To use STL queue in bfs, dijkstra's, maxflow
+#include <cstring> // To use memset()
+#include <cstdlib> // To use atoi(), abs(), malloc()
+#include <algorithm> // To use sort(), next_permutation(), min(), max() etc.
+#include <unordered_map> // To allow O(1) mapping access using key->value
+#include <set> // To sort and remove duplicate when inserted
+#include <unordered_set> //To remove duplicates and count size
+#include <fstream> //To use c++ style read stream from file
+
+//macro here
+#define FOR(i,a,b) for(size_t i=a;i<b;i++)
+#define DE(x) cout << #x << ":" << x << endl
+#define DEIF cout<<"DEBUG: in if"<<endl
+#define DEELSE cout<<"DEBUG: in else"<<endl
+#define DEIFIF cout<<"DEBUG: in if if"<<endl
+#define DEIFELSE cout<<"DEBUG: in if else"<<endl
+#define DEELSEIF cout<<"DEBUG: in else if"<<endl
+#define DEELSEELSE cout<<"DEBUG: in else else"<<endl
+#define ALL(a) a.begin(),a.end()
+#define CINLINE(a) getline(cin,a)
+#define FILL(a,b) memset(a, b , sizeof(a)) //fill array a with all bs
+#define INIT(a) FILL(a,0) //initialize array a with all 0s
+
+//name space here
+using namespace std;
+
+//typedef here
+typedef vector<int> VI;
+typedef vector<pair<int,pair<int,int>>> Edges;
+typedef pair<int,int> PII;
+
+//debugging functions here
+template<class T>
+void DEVEC(vector<T> vec){
+    FOR(i,0,vec.size()){
+        DE(vec[i]);
+    }
+    cout<<endl;
+}
+
+template<class T>
+void DEARRAY(T array[],int num=20){
+    FOR(i,0,num+1){
+        cout<<array[i]<<" ";
+    }
+    cout<<endl;
+}
+
+//functions, global variables, comparators & Non-STL Data Structures definition here
+
+
+//start of main()
+int main(int argc, const char * argv[]) {
+    //Your code here
+    
+    
+    return 0;
+}
