eb5461d52ff887b34c08747ab71299cccfbd2c25
SteveDengZishi <zd475@nyu.edu>
2017-11-05 01:05:07 -0400

    read & retrive info

--- Banker/optimistic.cpp
+++ Banker/optimistic.cpp
@@ -1,3 +1,7 @@
+//  Created by Steve DengZishi on 11/4/17.
+//  Copyright © 2017 Steve DengZishi. All rights reserved.
+//
+
 #include <iostream>
 #include <cstdio>
 #include <vector>
@@ -54,12 +58,111 @@ void DEARRAY(T array[],int num=20){
 }
 
 //functions, global variables, comparators & Non-STL Data Structures definition here
+string fileName;
+fstream inFile;
+int cycle;
+int waitTime;
+int taskNum;
+int typeNum;
+int* resources;
+vector<string>* instructions;
+int* termination;
 
+struct result{
+    int first,second;
+    double third;
+    
+    result(){}
+    result(int a, int b, double c):first(a),second(b),third(c){}
+};
+
+result* recordTable;
+
+bool checkEnd(){
+    bool end=true;
+    for(int i=0;i<taskNum;i++){
+        if(termination[i]==0) end=false;
+    }
+    return end;
+}
+
+string fetchEffectiveInstruction(int i){
+    if(termination[i]!=1) return instructions[i][cycle];
+    else return "void";
+}
 
 //start of main()
 int main(int argc, const char * argv[]) {
-    //Your code here
+    //Taking command line argument
+    if(argc==2){
+        fileName=argv[1];
+    }
+    else{
+        cerr<<"illegal input, you should provide only one command line arguments with the fileName"<<endl;
+        exit(0);
+    }
     
+    //open input file
+    inFile.open(fileName,ios::in);
+    inFile>>taskNum>>typeNum;
+    
+    //dynamic allocation of array of resources in system
+    resources = new int[typeNum];
+    for(int i=0;i<typeNum;i++){
+        inFile>>resources[i];
+    }
+    //dynamic allocation of array of string instructions and termination status for all tasks
+    instructions = new vector<string>[taskNum];
+    termination = new int[taskNum]{};
+    recordTable = new result[taskNum];
+    
+    //divide and store instruction lines according to their taskNum
+    string instruction;
+    getline(inFile,instruction);
+    while(getline(inFile,instruction)){
+        //cout<<instruction<<endl;
+        instructions[(instruction[10]-'0')-1].push_back(instruction);
+    }
+    //initialized and start cycle 0
+    cycle=0;
+    while(true){
+        //if all tasks have terminated, break out of the infinite loop
+        if(checkEnd()==true) break;
+        //run one instruction for each task in every round
+        for(int i=0;i<taskNum;i++){
+            instruction = fetchEffectiveInstruction(i);
+            if(instruction!="void"){
+                //execute this instruction
+                string order=instruction.substr(0,instruction.find(' '));
+                int taskId=instruction[10]-'0';
+                int resourceType=instruction[12]-'0';
+                int resourceQuantity=instruction[14]-'0';
+                
+                //check what is the instruction
+                if(order=="initiate"){
+                    
+                }
+                else if(order=="request"){
+                    
+                }
+                else if(order=="release"){
+                    
+                }
+                else if(order=="compute"){
+                    
+                }
+                else if(order=="terminate"){
+                    //terminate task
+                    termination[taskId]=1;
+                    //record results
+                    
+                }
+            }
+        }
+        //increment cycle
+        cycle++;
+    }
+    //after execution, print output in the given format
     
     return 0;
 }
